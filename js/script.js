b66ef9f9ee9659d722a03b8f77945998409356cb
Maxsuel Fernando <maxsuel.dev@gmail.com>
2023-10-05 17:21:02 -0300

    check validity com mensagens

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+import eUmCpf from './validaCpf.js';
+import eMaiorDeIdade from './validaIdade.js';
+
 const formulario = document.querySelector("[data-formulario]");
 const camposForm = document.querySelectorAll("[required]");
 
@@ -12,7 +15,8 @@ formulario.addEventListener('submit', (event) => {
         "aniversario": event.target.elements["aniversario"].value
     }
 
-    console.log(listaRespostas);
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+    window.location.href = "./index.html";
 })
 
 camposForm.forEach(campo => {
@@ -20,6 +24,68 @@ camposForm.forEach(campo => {
     campo.addEventListener('invalid', event => event.preventDefault());
 });
 
-function validaCampo(campo) {
-    
+const tiposDeErro = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "tooShort",
+    "customError"
+];
+
+const mensagens = {
+    nome: {
+        valueMissing: "O campo de nome não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um nome válido.",
+        tooShort: "Por favor, preencha um nome válido."
+    },
+    email: {
+        valueMissing: "O campo de e-mail não pode estar vazio.",
+        typeMismatch: "Por favor, preencha um email válido.",
+        tooShort: "Por favor, preencha um e-mail válido."
+    },
+    rg: {
+        valueMissing: "O campo de RG não pode estar vazio.",
+        patternMismatch: "Por favor, preencha um RG válido.",
+        tooShort: "O campo de RG não tem caractéres suficientes."
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        patternMismatch: "Por favor, preencha um CPF válido.",
+        customError: "O CPF digitado não existe.",
+        tooShort: "O campo de CPF não tem caractéres suficientes."
+    },
+    aniversario: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    termos: {
+        valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
+    }
 }
+
+function validaCampo(campo) {
+    let mensagem = "";
+
+    if(campo.name == 'cpf' && campo.length >= 11) {
+        eUmCpf(campo);
+    }
+
+    if(campo.name == 'aniversario' && campo.value != "") {
+        eMaiorDeIdade(campo);
+    }
+
+    tiposDeErro.forEach(erro => {
+        if(campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+        }
+    });
+
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const inputValidation = campo.checkValidity();
+
+    if(!inputValidation) {
+        mensagemErro.textContent = mensagem;
+    } else {
+        mensagemErro.textContent = "";
+    }
+}   
